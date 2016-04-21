e0f7d8176c6757e4ba5a47a2655876aa456fc155
Ryan Brady <rdbrady@gmail.com>
2016-04-20 20:35:37 -0700

    used onet skill requirements to get midskill table

---
+++ R/mid_skill.R
@@ -0,0 +1,12 @@
+# if not using Rstudio project:
+# setwd("..")
+
+attach(onet)
+
+education.reqs <- subset(`Education, Training, and Experience`, Scale.ID == 'RL') %>% 
+  group_by(O.NET.SOC.Code, Element.Name) %>% 
+  dplyr::summarize(value = weighted.mean(Category, w = Data.Value))
+
+education.reqs$mid.skill <- education.reqs$value >=2 & education.reqs$value <=6
+education.reqs$Element.Name <- NULL
+saveRDS(education.reqs, "data/processed/mid_skill.R")
