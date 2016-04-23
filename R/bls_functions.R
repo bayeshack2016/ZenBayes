1bfef4918969828aee45a9256dc7033e24028ab9
prakashn2083 <prakash.natraj@gmail.com>
2016-04-23 13:19:55 -0700

    Adding SOC-> Title function

---
+++ R/bls_functions.R
@@ -0,0 +1,13 @@
+library(data.table)
+
+get.onet.data.table<-function(path){
+  df<-data.table(read.csv(path, sep = "\t"));
+  setkey(df,'O.NET.SOC.Code', 'Title')  
+  return(df)
+}
+
+
+get.title<-function(bls.sec.code, onet.data){
+return(df[O.NET.SOC.Code %like% bls.sec.code, Title])
+}
+
