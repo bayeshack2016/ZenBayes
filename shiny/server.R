3d41081fe00cf37d4262cef6cd481dc9e69db73f
ankur-gupta <ankur@perfectlyrandom.org>
2016-04-24 03:16:46 -0700

    Adding code to show scoring results in shiny Also chanaging paths to data files

--- shiny/server.R
+++ shiny/server.R
@@ -16,9 +16,9 @@ source("../R/bls_functions.R")
 initialize<-function(){
   bls <- readRDS("../data/processed/bls_project_gain_loss_sorted_titles.rds")
   biggest.loss <- subset(bls, employment.2014 - employment.2024 > 10000)
-  
+
   biggest.loss$abb.title = str_wrap( biggest.loss$title, width = 40)
-  
+
   return(biggest.loss)
 
 }
@@ -52,42 +52,83 @@ graph.data$Related.O.NET.SOC.Code <- gsub("[.][0-9]{2}", "", graph.data$Related.
 graph.data <- unique(graph.data[graph.data[,1] != graph.data[,2],1:2])
 # Takes no time.
 career.graph <- graph.data.frame(graph.data)
+
+#---------------------------------------------------------------------------------
+# Load datasets and files for scoring functions.
+#---------------------------------------------------------------------------------
+# This is a smaller dataset (~1.2 MB) that contains averaged data from ONET.
+cat("Loading data/processed/onet_combined_averaged.RDS ... ")
+onet.comb.avg <- readRDS("data/processed/onet_combined_averaged.RDS")
+cat("done\n")
+
+# Load the score.df for all the top 30 biggest losers (from 2014 to 2024)
+cat("Loading data/processed/top-losers-score-df-list.rds ... ")
+top.losers.score.df.list <- readRDS("data/processed/top-losers-score-df-list.rds")
+cat("done\n")
+
+# Read the file containing functions for scoring.
+cat("Sourcing R/onet-skill-difference-functions.R ...")
+source("R/onet-skill-difference-functions.R")
+cat("done\n")
+
+#---------------------------------------------------------------------------------
+# Set the working directory correctly and print final message
+#---------------------------------------------------------------------------------
 setwd("shiny")
+cat("\n---------------------------------------\n")
+cat("\nFinished loading data and files. Ready!\n")
+cat("\n---------------------------------------\n")
 
 
 
 
 
 shinyServer(function(input, output) {
- 
-  ## One time graph initialization   
+
+  ## One time graph initialization
   print(names(biggest.loss))
-  
+
   output$overallplot <- renderPlot({
     biggest.loss$raw.losses <- biggest.loss$employment.2014 - biggest.loss$employment.2024
     biggest.loss.new<- biggest.loss[order(biggest.loss$raw.losses),]
-    ggplot( biggest.loss.new[1:20,], aes(x = abb.title, y = raw.losses)) + geom_bar(stat = "identity",fill="steelblue") + 
+    ggplot( biggest.loss.new[1:20,], aes(x = abb.title, y = raw.losses)) + geom_bar(stat = "identity",fill="steelblue") +
       coord_flip()  + ylab("Number of jobs lost") + xlab("") +
       theme(axis.text=element_text(size=12), axis.title.x=element_text(size=18))# + theme(axis.text.y = element_text(angle = 90, hjust = 1))
   })
-  
-  
+
+
   output$jobdist<-renderPlot({
    print(input$select)
-   barplot(c(biggest.loss[as.numeric(input$select),]$employment.2014, biggest.loss[as.numeric(input$select),]$employment.2024), 
+   barplot(c(biggest.loss[as.numeric(input$select),]$employment.2014, biggest.loss[as.numeric(input$select),]$employment.2024),
             main = sprintf("Jobs lost =  %d",biggest.loss[as.numeric(input$select),]$employment.2014 - biggest.loss[as.numeric(input$select),]$employment.2024 ),names.arg = c("2014", "2024"))
-    
+
   })
 
   output$jobgraph<-renderPlot({
     input$select
     plot.subgraph(career.graph = career.graph, SOC = biggest.loss[as.numeric(input$select),]$SOC.Code)
-    
-    
+
+
   })
-  
+
+
+  # Scoring Table
   output$table <- renderDataTable(biggest.loss)
-  
-  
-  
+  output$table <- renderDataTable({
+    cat("\n---------------------------------------\n")
+    cat("Logging renderDataTable():")
+    cat("\n---------------------------------------\n")
+    cat(paste0("input$select = ", input$select, "\n"))
+    cat("User selected:\n")
+    cat(paste0("\tSOC.Code: ", biggest.loss$SOC.Code[as.numeric(input$select)], "\n"))
+    cat(paste0("\tTitle: ", biggest.loss$title[as.numeric(input$select)], "\n"))
+
+    # Obtain the output dataframe that contains scores against the user-selected job.
+    score.df <- get.closest.df(as.character(biggest.loss$SOC.Code[as.numeric(input$select)]),
+                               onet.comb.avg, top.losers.score.df.list)
+    return(score.df)
+  })
+
+
+
 })
--- shiny/ui.R
+++ shiny/ui.R
@@ -9,34 +9,33 @@ library(shiny)
 source("../R/bls_functions.R")
 
 
-  bls <- readRDS("/Users/prakash/bayeshack2016/data/processed/bls_project_gain_loss_sorted_titles.rds")
+  bls <- readRDS("../data/processed/bls_project_gain_loss_sorted_titles.rds")
   biggest.loss <- subset(bls, employment.2014 - employment.2024 > 10000)
-titles<-lapply(biggest.loss$SOC.Code, get.titles, 
-               onet.data = get.onet.data.table("/Users/prakash/bayeshack2016/data/O_NET/Occupation Data.txt"));
+titles<-lapply(biggest.loss$SOC.Code, get.titles,
+               onet.data = get.onet.data.table("../data/O_NET/Occupation Data.txt"));
 choice.list <-seq(1, length(titles))
 names(choice.list) <- titles
 
 shinyUI(fluidPage(theme = "darkly.css",
                   navbarPage("The fastest shrinking careers in the US",
-                             tabPanel("30 fastest declining occupations", titlePanel(h1("Occupations in danger!")), 
+                             tabPanel("30 fastest declining occupations", titlePanel(h1("Occupations in danger!")),
                                       mainPanel(plotOutput("overallplot", width = "150%", height = "1100px"))),
                              tabPanel("Occupation specific outlook",  titlePanel(h1("Loss numbers")),   sidebarLayout(
-                               sidebarPanel(    selectInput("select", label = h3("Select job title"), 
+                               sidebarPanel(    selectInput("select", label = h3("Select job title"),
                                                             choices = choice.list, selected = 1)
                                ),
-                               
-                               
+
+
                                mainPanel( tabsetPanel(
                                  tabPanel("Job loss", plotOutput("jobdist")),
                                  tabPanel("Job to move to", dataTableOutput("table")),
                                  tabPanel("Job graph", plotOutput("jobgraph"))
-                                 )) 
-                               
+                                 ))
+
                              )
-                             
-                             
-                             
-                             
-                             
+
+
+
+
+
                              ))))
-                  
